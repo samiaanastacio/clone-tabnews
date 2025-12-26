44e5e6f680b59789d07e37ba11f18a54992e76bf
Sâmia Anastacio <82920706+samiaanastacio@users.noreply.github.com>
2025-12-26 14:14:33 +0000

    Desafio curso

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 function Home() {
-    return <h1> Changing </h1>
+    return <h1> Este é um desafio do curso do Felipe Dechamps!
+        E eu dedico essa primeira página a você, meu amor! Obrigada por ser meu primeiro apoiador, mentor. Sem você eu não estaria trilhando esse caminho! Te amo! </h1>
 }
 
 export default Home;
