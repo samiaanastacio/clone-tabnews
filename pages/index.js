a3c488eb2d29cd01e8a283a374f8832cccc3eee9
Sâmia Anastacio <82920706+samiaanastacio@users.noreply.github.com>
2025-12-26 14:30:43 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home() {
     return <h1> Este é um desafio do curso do Felipe Dechamps!
-        E eu dedico essa primeira página a você, meu amor! Obrigada por ser meu primeiro apoiador, mentor. Sem você eu não estaria trilhando esse caminho! Te amo! </h1>
+        E eu dedico essa primeira página a você, meu amor! Obrigada por ser meu primeiro apoiador, mentor. Sem você eu não estaria trilhando esse caminho! Te amo! <3 </h1>
 }
 
 export default Home;
